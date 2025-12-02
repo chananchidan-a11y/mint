6f0a909c13cad37f1b93c918ac2786b54efb53d2
chananchidan-a11y <chananchida.n@kkumail.com>
2025-12-02 11:16:15 +0700

    Chananchida Nammoontri

---
+++ mint/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("my name is Chananchida Nammoontri ");
+            Console.WriteLine("student id 6834501866");
+            Console.WriteLine("major: Computer and Information Science");
+            Console.WriteLine("faculty: Is");
+            Console.WriteLine("khon kaen university ");
+        }
+    }
+    }
+}
